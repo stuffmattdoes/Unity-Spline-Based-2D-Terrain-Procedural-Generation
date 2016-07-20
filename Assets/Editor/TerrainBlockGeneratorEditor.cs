0df2a20a9894e78832c831b8a5a7c74cac7100d6
Matthew J. Morrison <m.james.morrison00@gmail.com>
2016-07-20 17:37:55 -0400

    Starting on the terrain block generator custom inspector

---
+++ Assets/Editor/TerrainBlockGeneratorEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainBlockGenerator))]
+public class TerrainBlockGeneratorEditor : Editor {
+
+	void OnInspectorGUI() {
+		TerrainBlockGenerator myTarget = (TerrainBlockGenerator)target;
+//		myTarget.blocksPerSetMin = EditorGUILayout.IntField("Test", myTarget.blocksPerSetMin);
+	}
+}
--- Assets/Scripts/TerrainBlockGenerator.cs
+++ Assets/Scripts/TerrainBlockGenerator.cs
@@ -3,38 +3,57 @@ using System.Collections.Generic;
 
 // TODO
 // - (DONE) Transform endpoint from spline terrain local to world coordinates
-//	^ Endpoints get screwy when going from large piece to smaller piece
-//	^ Turns out, the endpoint values was incorrect in the prefabs. UGH!
+//		^ Endpoints get screwy when going from large piece to smaller piece
+//		^ Turns out, the endpoint values was incorrect in the prefabs. UGH!
+// - Spawn blocks in random order
+//		^ If blocks to spawn > blocks available, recycle a few
+// - Implement bione class/struct to hold blocks & spawning properties for each block
+//		^ Should have sliders to influence randomized probability & spawning, like terrain spline generator
 
 public class TerrainBlockGenerator : MonoBehaviour {
 
-	public int blocksToSpawn = 3;
+	public int blocksPerSetMin = 3;
+	public int blocksPerSetMax = 10;
 	public List<GameObject> terrainBlocks;
 
 	// Variables
-//	private Vector3 nextSpawnPoint;
-//	private Vector3 lastStartPoint;
-	private Vector3 lastEndPoint;
+	private Vector3 nextSpawnPoint;
+
+	[System.Serializable]
+	public class TerrainBlockSet {
+
+		public GameObject terrainBlock;
+
+		// Quantity of object to pre-instantiate
+		[Range(0, 10)]
+		public int probability;
+
+	}
+
+	public List<TerrainBlockSet> terrainBlockSet = new List<TerrainBlockSet>();
 
 	// Use this for initialization
 	void Start () {
-//		lastStartPoint = transform.position;
-		lastEndPoint = transform.position;
+		ResetBlocks ();
 		PlaceBlocks ();
+	}
+
+	private void ResetBlocks() {
+		nextSpawnPoint = transform.position;
 	}
 	
 	private void PlaceBlocks() {
 		GameObject newTerrainBlock;
 		SplineTerrainGenerator splineProps;
 
-		for (int i = 0; i < blocksToSpawn; i ++) {
+		for (int i = 0; i < blocksPerSetMin; i ++) {
 
-			newTerrainBlock = (GameObject)Instantiate(terrainBlocks[i], lastEndPoint, Quaternion.identity);
+			newTerrainBlock = (GameObject)Instantiate(terrainBlocks[i], nextSpawnPoint, Quaternion.identity);
 			newTerrainBlock.transform.parent = gameObject.transform;
 			splineProps = terrainBlocks[i].GetComponentInChildren<SplineTerrainGenerator> ();
 
-			lastEndPoint = newTerrainBlock.transform.position + splineProps.endPoint;
-			Debug.Log (splineProps.endPoint + ", " + lastEndPoint);
+			nextSpawnPoint = newTerrainBlock.transform.position + splineProps.endPoint;
+//			Debug.Log (splineProps.endPoint + ", " + lastEndPoint);
 
 
 		}
